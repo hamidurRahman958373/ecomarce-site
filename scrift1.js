68e4aeccf77cec38704deb41a89da666d476b8be
unknown <hamidurshohag9583@gmail.com>
2020-09-19 11:48:51 +0600

    first commit

---
+++ scrift1.js
@@ -0,0 +1,15 @@
+  var productImg = document.getElementById("productImg");
+  var smallimg = document.getElementByClassName("smallimg");
+     smallimg[0].onclick = function(){
+		 productImg.src = smallimg[0].src;
+	 }
+	 smallimg[1].onclick = function(){
+		 productImg.src = smallimg[1].src;
+	 }
+	 smallimg[2].onclick = function(){
+		 productImg.src = smallimg[2].src;
+	 }
+	 smallimg[3].onclick = function(){
+		 productImg.src = smallimg[3].src;
+	 }
+	 
